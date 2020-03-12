94d64bbc6288dfd101429c88d4283e11f9ce2610
EO-1 <eolson0150@gmail.com>
2020-03-12 14:52:21 -0500

    Add files via upload

--- match_cityscapes_to_minneapolis.py
+++ match_cityscapes_to_minneapolis.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from PIL import Image
+from shutil import copyfile
 
 help_msg = """
 Processing Script to match Cityscapes data to the minneapolis data.
@@ -18,39 +19,26 @@ def match_cityscapes(msp_dir, cs_dir, output_dir, phase):
     os.makedirs(savedir + '_MSP', exist_ok=True)
     print("Directory structure prepared at %s" % output_dir)
     
+
     msp_map_expr = msp_dir + "/*.jpeg"
     msp_map_paths = glob.glob(msp_map_expr)
     msp_map_paths = sorted(msp_map_paths)
     cs_expr = cs_dir + "/*/*_leftImg8bit.png"
-    print(cs_expr)
     cs_paths = glob.glob(cs_expr)
     cs_paths = sorted(cs_paths)
-    print(cs_paths)
 
-    for i, (msp_map_path) in enumerate(zip(msp_map_paths)):
-        for j, (cs_path) in enumerate(zip(cs_paths)):
-            print(msp_map_path)
-            print(cs_path)
+    #for i, (msp_map_path) in enumerate(zip(msp_map_paths)):
+    #    for j, (cs_path) in enumerate(zip(cs_paths)):
+    # NOTE: This mapping for this function will need to be changed.
+    # ATM it just goes in order.  There needs to be some logic being these pairings.
+    for i, (msp_map_path, cs_path) in enumerate(zip(msp_map_paths, cs_paths)):
+            msp_save_dir = os.path.join(savedir + "_MSP","%d.jpg" % i)
+            cs_save_dir = os.path.join(savedir + "_CITYSCAPES","%d.jpg" % i)
+            copyfile(msp_map_path, msp_save_dir)
+            copyfile(cs_path, cs_save_dir)
+            if i % (len(msp_map_paths) // 10) == 0:
+                print("%d / %d: last image saved at %s, " % (i, len(msp_map_paths), savedir))
 
-       # check_matching_pair(segmap_path, photo_path)
-       # segmap = load_resized_img(segmap_path)
-        #photo = load_resized_img(photo_path)
-
-        #data for pix2pix where the two images are placed side-by-side
-        ##sidebyside = Image.new('RGB', (512, 256))
-        #sidebyside.paste(segmap, (256, 0))
-        #sidebyside.paste(photo, (0, 0))
-        #savepath = os.path.join(savedir, "%d.jpg" % i)
-        #sidebyside.save(savepath, format='JPEG', subsampling=0, quality=100)
-
-        #data for cyclegan where the two images are stored at two distinct directories
-        #savepath = os.path.join(savedir + 'A', "%d_A.jpg" % i)
-        #photo.save(savepath, format='JPEG', subsampling=0, quality=100)
-        #savepath = os.path.join(savedir + 'B', "%d_B.jpg" % i)
-        #segmap.save(savepath, format='JPEG', subsampling=0, quality=100)
-        
-        #if i % (len(segmap_paths) // 10) == 0:
-        #    print("%d / %d: last image saved at %s, " % (i, len(segmap_paths), savepath))
 
 
 if __name__ == '__main__':
